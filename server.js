591e53a316f50117d1269cbd6be09bd4a60408da
CDkaisokuvn <chu.duong.dev@gmail.com>
2021-07-30 18:07:44 +0200

    them '/' vao truoc css trong file ejs. catch 404 phai o duoi cung sau router

--- server.js
+++ server.js
@@ -1,48 +1,49 @@
-const express = require('express');
-const morgan = require('morgan'); //log view request detail to server
-const config = require('config');
-const mongo = require('./config/mongo');
-var cors = require('cors');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const morgan = require("morgan"); //log view request detail to server
+const config = require("config");
+const mongo = require("./config/mongo");
+var cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 //const expressLayouts = require('express-ejs-layouts');
-const session = require('express-session');
-const flash = require('connect-flash');
-const passport = require('passport');
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
 
 const app = express();
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-let isDev = process.env.NODE_ENV !== 'production';
-app.locals.env = process.env.NODE_ENV || 'dev';
+let isDev = process.env.NODE_ENV !== "production";
+app.locals.env = process.env.NODE_ENV || "dev";
 
 //app.use(expressLayouts);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static('uploads'));
-app.use(express.static('public')); //static rsc from public folder
-app.set('view engine', 'ejs');
-app.set('views', './views'); // html client side from views folder
-
+app.use(express.static("uploads"));
+app.use(express.static("public")); //static rsc from public folder
+app.set("view engine", "ejs");
+app.set("views", "./views"); // html client side from views folder
 
 mongo.connect();
 app.use(cors());
 
-app.use(express.urlencoded({
-        extended: true
-    }),
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
 );
 app.use(express.json());
 
-app.use(session({
-        cookie: {
-            maxAge: 1000 * 60 * 60 * 24 * 30
-        }, //30 days
-        secret: config.get('jwtSecret'),
-        resave: true,
-        saveUninitialized: true
-    })
+app.use(
+  session({
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+    }, //30 days
+    secret: config.get("jwtSecret"),
+    resave: true,
+    saveUninitialized: true,
+  })
 );
 //------------ Passport Middlewares ------------//
 app.use(passport.initialize());
@@ -53,20 +54,15 @@ app.use(flash());
 
 //------------ Global variables ------------//
 
+const home = require("./routes/index");
+const auth = require("./routes/auth");
+app.use("/", home);
+app.use("/auth", auth);
 
 // catch 404 and forward to error handler
-    app.use(function (req, res, next) {
-        var err = new Error('Not Found');
-        err.status = 404;
-        next(err);
-      });
-  
-
-const home = require('./routes/index');
-const auth = require('./routes/auth');
-app.use('/',home);
-app.use('/auth', auth);
+app.use(function (req, res, next) {
+  res.render("404");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server running on PORT ${PORT}`));
-
