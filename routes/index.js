ab8b16797ba3dca5a7955a99dcd77fa5f921c870
vuchungbt <dinhmenhbt096@gmail.com>
2021-07-31 00:34:38 +0700

    new nav and footer

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,18 @@ router.get('/', (req, res) => {
     res.render('welcome');
 });
 
+//------------ blog list ------------//
+router.get('/bloglist', (req, res) => {
+    res.render('bloglist');
+});
+
+//------------ blog list ------------//
+router.get('/stories', (req, res) => {
+    res.render('stories');
+});
+
+
+
 router.get('/dashboard', ensureAuthenticated, (req,res) => res.render('home',{name: req.user.name}));
 
 module.exports = router;
